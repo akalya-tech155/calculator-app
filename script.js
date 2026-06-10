6902f194e4bdaa3f167062dab673e0d79b674d3a
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-10 18:24:34 +0530

    created new file

---
+++ script.js
@@ -0,0 +1,6 @@
+function getNumbers() {
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+
+    return { num1, num2 };
+}
